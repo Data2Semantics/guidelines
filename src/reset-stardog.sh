311ec818f4a565636545c692ba1f7629a4219388
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2014-09-16 13:25:08 +0200

    Now setting reasoning graph in stardom

--- src/reset-stardog.sh
+++ src/reset-stardog.sh
@@ -4,6 +4,10 @@ echo "Removing data from stardog"
 stardog data remove --all guidelines
 
 echo "Loading TMR and TMR4I into stardog"
-stardog data add --named-graph http://www.semanticweb.org/veruskacz/ontologies/2014/8/TMR.owl guidelines data/TMR.owl
-stardog data add --named-graph http://www.semanticweb.org/veruskacz/ontologies/2014/8/TMR4I.owl guidelines data/TMR4I.owl
+stardog data add --named-graph http://guidelines.data2semantics.org/vocab guidelines data/TMR.owl
+stardog data add --named-graph http://guidelines.data2semantics.org/vocab guidelines data/TMR4I.owl
 
+echo "Setting http://guidelines.data2semantics.org/vocab as TBox graph"
+stardog-admin db offline guidelines
+stardog-admin metadata set -o reasoning.schema.graphs=http://guidelines.data2semantics.org/vocab reasoning.punning.enabled=true guidelines
+stardog-admin db online guidelines
