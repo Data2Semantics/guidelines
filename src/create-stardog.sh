5480c7cd2250538e3a33ad79e7e437e1457f08b1
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2014-09-16 14:32:45 +0200

    Moved setting properties of database and initial data loading to create-stardog.sh

--- src/create-stardog.sh
+++ src/create-stardog.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-stardog-admin db create -n guidelines
+stardog-admin db create -n guidelines data/TMR.owl data/TMR4I.owl
+
+echo "Setting tag:stardog:api:context:all as TBox graph"
+# Other option: http://guidelines.data2semantics.org/vocab
+stardog-admin db offline guidelines
+stardog-admin metadata set -o reasoning.schema.graphs=tag:stardog:api:context:all guidelines
+stardog-admin db online guidelines
--- src/reset-stardog.sh
+++ src/reset-stardog.sh
@@ -7,7 +7,3 @@ echo "Loading TMR and TMR4I into stardog"
 stardog data add --named-graph http://guidelines.data2semantics.org/vocab guidelines data/TMR.owl
 stardog data add --named-graph http://guidelines.data2semantics.org/vocab guidelines data/TMR4I.owl
 
-echo "Setting http://guidelines.data2semantics.org/vocab as TBox graph"
-stardog-admin db offline guidelines
-stardog-admin metadata set -o reasoning.schema.graphs=http://guidelines.data2semantics.org/vocab reasoning.punning.enabled=true guidelines
-stardog-admin db online guidelines
