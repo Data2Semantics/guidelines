0245e3a025134d8d24e6e61170f4af3e46ae187f
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2014-10-09 13:16:46 +0200

    Drugbank excerpt (only drugs, same-as links and drug-interactions)

--- src/load-drugbank.sh
+++ src/load-drugbank.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 echo "Loading drugbank data into stardog"
-stardog data add --named-graph http://www4.wiwiss.fu-berlin.de/drugbank guidelines data/drugbank_dump.nt
+# stardog data add --named-graph http://www4.wiwiss.fu-berlin.de/drugbank guidelines data/drugbank_dump.nt
+stardog data add guidelines data/drugbank_small.nt
